2ac999f756ed8393892d2efdfa186b5b5504604a
Md Jubayer <mdjubayerislamshuvo34@gmail.com>
2025-11-08 13:46:48 +0600

    qe

--- self-deal/app/api/create-otp/route.tsx
+++ self-deal/app/api/create-otp/route.tsx
@@ -4,14 +4,18 @@ import { connectDB } from "@/lib/mongodb";
 import sendOTPEmail from "@/lib/emailOtp";
 
 export async function POST(req: Request) {
-  const { email } = await req.json();
-  await connectDB();
-
-  const user = await User.findOne({ email });
-  if (!user || user?.isEmailVerified)
-    return NextResponse.json({ error: "Wrong request" }, { status: 404 });
-
-  sendOTPEmail({ to: email, userName: user.username, expiryMinutes: 10 });
-
-  return NextResponse.json({ success: true }, { status: 200 });
+  try {
+    const { email } = await req.json();
+    await connectDB();
+  
+    const user = await User.findOne({ email });
+    if (!user || user?.isEmailVerified)
+      return NextResponse.json({ error: "Wrong request" }, { status: 404 });
+  
+    await sendOTPEmail({ to: email, userName: user.username, expiryMinutes: 10 });
+  
+    return NextResponse.json({ success: true }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error || "Failed to send OTP" }, { status: 500 });
+  }
 }
