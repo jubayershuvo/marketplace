8a06fefd25f37cb99eee886d458932172edce5d9
Md Jubayer <mdjubayerislamshuvo34@gmail.com>
2025-11-15 21:52:46 +0600

    sad

--- bdris/app/api/correction/download-pdf/[id]/route.ts
+++ bdris/app/api/correction/download-pdf/[id]/route.ts
@@ -23,7 +23,7 @@ export async function GET(
       );
     }
 
-    const targetURL = application.printLink;
+    const targetURL = application.printLink ||'';
 
     browser = await puppeteer.launch({
       headless: true,
